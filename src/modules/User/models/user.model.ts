2ff3c6fa173bad13bb790d88df756543c8f618f5
Thach <ducthach.1401@gmail.com>
2021-07-21 11:57:16 +0700

    update

--- src/modules/User/models/user.model.ts
+++ src/modules/User/models/user.model.ts
@@ -1,16 +1,19 @@
 export interface createUser {
     id:string,
     name: string,
+    gender: string,
     age: number,
     DOB: Date,
     username: string,
     password: string,
+    password_confirm: string,
     location: string
 }
 
 export interface updateUser {
     id: string,
     name: string,
+    gender: string,
     age: number,
     DOB: Date,
     username: string,
--- src/modules/User/routes/user.route.ts
+++ src/modules/User/routes/user.route.ts
@@ -1,6 +1,7 @@
 import express, { Application, Router, Request, Response } from "express";
 import { UserController } from "../controllers/user.controller";
 import { authenticateToken } from "../../../middlewares/auth.middleware";
+import { commonValidateBody } from "../../../middlewares/validate.middleware";
 
 export class UserRoute {
     public userController: UserController = new UserController();
