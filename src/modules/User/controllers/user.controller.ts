7aee44acce690f8a9cb789bc69b82ea9c3ad9842
peternguyennvt <thuan156109@gmail.com>
2021-07-21 08:42:52 +0700

    Update controlller

--- src/modules/User/controllers/user.controller.ts
+++ src/modules/User/controllers/user.controller.ts
@@ -1,3 +1,25 @@
+import { UserService } from "../services/user.service";
+import { Request, Response } from "express"
+import { IUser } from "../models/user.model";
+import { Schema, model } from "mongoose";
+import User from "../../../common/entity/user.entity";
+
 export class UserController {
+    public userService: UserService = new UserService(User);
     
+    async login(req: Request, res: Response) {
+        const username = req.body.username;
+        const token = this.userService.createToken({name: username});
+        res.json(token);
+    }
+
+    public getAllUsers = async (req: Request, res: Response) => {
+        try {
+            const data = await this.userService.getUsers();
+            res.json({Users: data});
+        }
+        catch(err) {
+            throw err;
+        }
+    }
 }
