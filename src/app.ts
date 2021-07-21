a71db90af88cc985274b0c25678d9aed783fd2c7
peternguyennvt <thuan156109@gmail.com>
2021-07-21 08:13:15 +0700

    create routes

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,38 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, Application } from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import { UserRoute } from "./modules/User/routes/user.route";
+import User from "./common/entity/user.entity";
 
-try {
-    mongoose.connect(`${process.env.MONGO_URL}`, {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-        useCreateIndex: true, 
-        useFindAndModify: false
-    });
-
-    const app = express();
-
-    // app.use('/v1/users', )
-
-    app.listen(`${process.env.PORT}`, (): void => {
-        console.log("Server is running at http://localhost:5000");
-    });
+async function run () {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}`, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true, 
+            useCreateIndex: true, 
+            useFindAndModify: false
+        });
+        
+    
+        const app: Application = express();
+    
+        const userRoute: UserRoute = new UserRoute();
+        
+    
+        userRoute.routes(app);
+        // app.get('/', async (req: Request, res: Response) => {
+        //     const result = await User.find();
+        //     res.send(result);
+        // })
+    
+        app.listen(`${process.env.PORT}`, (): void => {
+            console.log("Server is running at http://localhost:5000");
+        });
+    }
+    
+    catch(error) {
+        throw error;
+    }
 }
 
-catch(error) {
-    throw error;
-}
+run();
--- src/modules/User/routes/user.route.ts
+++ src/modules/User/routes/user.route.ts
@@ -1,7 +1,13 @@
-import express, { Router } from "express";
+import express, { Application, Router, Request, Response } from "express";
+import { UserController } from "../controllers/user.controller";
+import { authenticateToken } from "../../../middlewares/auth.middleware";
 
 export class UserRoute {
-    public routes() {
+    public userController: UserController = new UserController();
+
+    public routes = (app: Application) => {
+        app.route('/v1/users')
+            .get(authenticateToken, this.userController.getAllUsers);
         
     }
 }
