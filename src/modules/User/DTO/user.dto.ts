16fac856a9fea7e001e2bd1b2a967ce24a82f121
Thach <ducthach.1401@gmail.com>
2021-07-22 11:32:06 +0700

    update

--- src/modules/User/DTO/user.dto.ts
+++ src/modules/User/DTO/user.dto.ts
@@ -1,7 +1,6 @@
 import Joi from 'joi';
 
 export const createUserSchema = Joi.object({
-    id: Joi.string().required(),
     name: Joi.string().required(),
     age: Joi.number().required(),
     DOB: Joi.date().required(),
@@ -9,24 +8,16 @@ export const createUserSchema = Joi.object({
     password: Joi.string().required(),
     password_confirm: Joi.string().required(),
     location: Joi.string(),
-    update_at: Joi.date(),
-    create_at: Joi.date()
 })
 
 export const updateUserSchema = Joi.object({
-    id: Joi.string().required(),
-    name: Joi.string().required(),
-    age: Joi.number().required(),
-    DOB: Joi.date().required(),
-    userName: Joi.string().required(),
-    password: Joi.string().required(),
-    password_confirm: Joi.string().required(),
+    name: Joi.string(),
+    age: Joi.number(),
+    DOB: Joi.date(),
+    userName: Joi.string(),
+    password: Joi.string(),
+    password_confirm: Joi.string(),
     location: Joi.string(),
-    update_at: Joi.date()
-})
-
-export const deleteUserSchema = Joi.object({
-    id: Joi.number().required()
 })
 
 export const searchUserSchema = Joi.object({
