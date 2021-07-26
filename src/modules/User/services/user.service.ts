bafc88f24df816a896d9ee19dc4e5330805a3376
Thach <ducthach.1401@gmail.com>
2021-07-26 10:21:48 +0700

    update

--- src/modules/User/services/user.service.ts
+++ src/modules/User/services/user.service.ts
@@ -63,6 +63,7 @@ export class UserService extends BaseRepository<IUser> {
             // const saltRounds = 10;
             // const encryptPassword = await bcrypt.hash(data.password, saltRounds);
             // data.password = encryptPassword;
+            data.isDelete = false;
             const newData = new User(data);
             await newData.save();
             return {
